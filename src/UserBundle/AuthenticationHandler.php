b58f801dc694c2defd678fddaa42d8935480398f
Daniel West <daniel@silverback.is>
2017-02-10 19:37:09 +0000

    User + Admin Login Functionality

---
+++ src/UserBundle/AuthenticationHandler.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace UserBundle;
+
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+use Symfony\Component\Translation\DataCollectorTranslator;
+use Symfony\Component\Security\Core\Role\RoleHierarchy;
+
+class AuthenticationHandler implements AuthenticationSuccessHandlerInterface, AuthenticationFailureHandlerInterface
+{
+	private $router,
+	$session,
+	$login_path,
+	$translator,
+	$admin_login_path;
+
+	public function __construct(RouterInterface $router, Session $session, DataCollectorTranslator $translator, $login_path, $admin_login_path)
+	{
+		$this->router  = $router;
+		$this->session = $session;
+		$this->login_path = $login_path;
+		$this->translator = $translator;
+		$this->admin_login_path = $admin_login_path;
+	}
+
+	public function onAuthenticationSuccess( Request $request, TokenInterface $token )
+	{
+		$nextURL = $this->login_path;
+		$roles = $token->getUser()->getRoles();
+		if(in_array('ROLE_ADMIN', $roles)){
+			$nextURL = $this->admin_login_path;
+		}
+
+		if( $request->isXmlHttpRequest() ){
+			$array = array( 
+				'href' => $nextURL
+			);
+			$response = new Response( json_encode( $array ), Response::HTTP_OK );
+			$response->headers->set( 'Content-Type', 'application/json' );
+			return $response;
+		}else{
+			return new RedirectResponse( $nextURL );
+		}
+	}
+
+	public function onAuthenticationFailure( Request $request, AuthenticationException $exception )
+	{
+		if ( $request->isXmlHttpRequest() ) {
+			$errorMessage = (string)$exception->getMessage();
+
+			$array = array( 
+				'message' => $this->translator->trans($errorMessage, array(), 'validators') 
+			);
+			$response = new Response( json_encode( $array ), Response::HTTP_UNAUTHORIZED );
+			$response->headers->set( 'Content-Type', 'application/json' );
+			return $response;
+		} else {
+			// set authentication exception to session
+			$request->getSession()->set(SecurityContextInterface::AUTHENTICATION_ERROR, $exception);
+			return new RedirectResponse( $this->router->generate( 'login_route' ) );
+		}
+	}
+}
---
+++ src/UserBundle/EventListener/RegisterListener.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace UserBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormErrorIterator;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+/**
+ * Listener responsible to change the redirection at the end of the password resetting
+ */
+class RegisterListener implements EventSubscriberInterface
+{
+    protected $serializer, $requestStack;
+
+    public function __construct($requestStack)
+    {
+        $encoders = array(new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $this->serializer = new Serializer($normalizers, $encoders);
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegisterSuccess',
+            FOSUserEvents::REGISTRATION_FAILURE => 'onRegisterFailure',
+        );
+    }
+
+    public function onRegisterSuccess(FormEvent $event){
+        $data = array();
+
+        //$rolesArr = array('ROLE_USER');
+        /** @var $user \FOS\UserBundle\Model\UserInterface */
+        //$user = $event->getForm()->getData();
+        //$user->setRoles($rolesArr);
+
+        $response = new Response($this->serializer->serialize($data, 'json'), Response::HTTP_CREATED);
+        $this->setResponse($response, $event);
+    }
+
+    public function onRegisterFailure(FormEvent $event){
+        $HTTPCode = Response::HTTP_BAD_REQUEST;
+
+        $request = $this->requestStack->getCurrentRequest();
+        //FormInterface
+        $form = $event->getForm();
+        $data = [];
+        $task = $request->request->get('task');
+        //would always fail registering on input validation because not all fields have been submitted - so let's check the field in question
+        if( $task['submit']=='no' ){
+            $fieldID = $task['input'];
+            $inputRef = explode("_",str_replace("fos_user_registration_form_", "", $fieldID));
+            $formField = $form;
+            foreach($inputRef as $inputRefPart){
+                $formField = $formField[$inputRefPart];
+            }
+            $currentError = $formField->getErrors()->current();
+            if($currentError){
+                $data[$fieldID] = $currentError->getMessage();
+            }else{
+                $HTTPCode = Response::HTTP_ACCEPTED;   
+            }
+                    
+        }else{
+            //FormErrorIterator
+            $errorIterator = $form->getErrors(true);            
+            while($errorIterator->valid()){
+                $currentError = $errorIterator->current();
+                $currentOrigin = $currentError->getOrigin();
+
+                $currentField = [ $currentOrigin->getName() ];
+                while( $p = $currentOrigin->getParent() ) {
+                    array_unshift($currentField, $p->getName() );
+                    $currentOrigin = $p;
+                }
+
+                $fieldID = join("_",$currentField);
+                
+                if(!isset($data[$fieldID])){
+                    $data[$fieldID] = $currentError->getMessage();
+                }
+                //$data[$fieldName][] = 
+                $errorIterator->next();
+            }
+        }
+        $response = new Response($this->serializer->serialize($data, 'json'), $HTTPCode);
+        $this->setResponse($response, $event);
+    }
+
+    private function setResponse(Response $response, FormEvent $event){
+        $response->headers->set('Content-Type', 'application/json');
+        $event->setResponse($response);
+    }
+}
