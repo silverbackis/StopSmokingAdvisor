5a4093a3b080017ef0ec49d633e9529d6e09df32
Daniel West <daniel@silverback.is>
2017-02-13 12:30:34 +0000

    oAuth set email and find by email when logging in in case already registered

--- src/UserBundle/Security/Core/User/FOSUBUserProvider.php
+++ src/UserBundle/Security/Core/User/FOSUBUserProvider.php
@@ -39,20 +39,27 @@ class FOSUBUserProvider extends BaseClass
         $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
         //when the user is registrating
         if (null === $user) {
+            $email = $response->getEmail();
+            if($email!==null){
+                //check if user with the email address exists
+                $user = $this->userManager->findUserByEmail($email);
+            }
+            if (null === $user) {
+                $user = $this->userManager->createUser();
+                $user->setPassword("");
+                $user->setEmail($email);
+                $user->setUsername($email);
+                $user->setEnabled(true);
+            }
+
             $service = $response->getResourceOwner()->getName();
             $setter = 'set'.ucfirst($service);
             $setter_id = $setter.'Id';
             $setter_token = $setter.'AccessToken';
-            // create new user here
-            $user = $this->userManager->createUser();
+
             $user->$setter_id($username);
             $user->$setter_token($response->getAccessToken());
-            //I have set all requested data with the user's username
-            //modify here with relevant data
-            $user->setUsername($username);
-            $user->setEmail($username);
-            $user->setPassword($username);
-            $user->setEnabled(true);
+            
             $this->userManager->updateUser($user);
             return $user;
         }
