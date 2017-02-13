077c0b0fab484d9ca566963b4b598870fb559eee
Daniel West <daniel@silverback.is>
2017-02-13 11:56:34 +0000

    Twitter oAuth configure

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -27,6 +27,10 @@ class User extends BaseUser
     protected $google_id;
     /** @ORM\Column(name="google_access_token", type="string", length=255, nullable=true) */
     protected $google_access_token;
+    /** @ORM\Column(name="twitter_id", type="string", length=255, nullable=true) */
+    protected $twitter_id;
+    /** @ORM\Column(name="twitter_access_token", type="string", length=255, nullable=true) */
+    protected $twitter_access_token;
     
     public function __construct()
     {
@@ -128,5 +132,53 @@ class User extends BaseUser
     public function getGoogleAccessToken()
     {
         return $this->google_access_token;
+    }
+
+    /**
+     * Set twitterId
+     *
+     * @param string $twitterId
+     *
+     * @return User
+     */
+    public function setTwitterId($twitterId)
+    {
+        $this->twitter_id = $twitterId;
+
+        return $this;
+    }
+
+    /**
+     * Get twitterId
+     *
+     * @return string
+     */
+    public function getTwitterId()
+    {
+        return $this->twitter_id;
+    }
+
+    /**
+     * Set twitterAccessToken
+     *
+     * @param string $twitterAccessToken
+     *
+     * @return User
+     */
+    public function setTwitterAccessToken($twitterAccessToken)
+    {
+        $this->twitter_access_token = $twitterAccessToken;
+
+        return $this;
+    }
+
+    /**
+     * Get twitterAccessToken
+     *
+     * @return string
+     */
+    public function getTwitterAccessToken()
+    {
+        return $this->twitter_access_token;
     }
 }
